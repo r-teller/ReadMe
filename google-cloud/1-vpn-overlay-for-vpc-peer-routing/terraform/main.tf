de102dbbb097dc84871c9f1d68523d54b68c606c
Robert Teller <robert.j.c.teller@gmail.com>
2021-02-26 15:09:41 -0800

    updating module names

--- google-cloud/1-vpn-overlay-for-vpc-peer-routing/terraform/main.tf
+++ google-cloud/1-vpn-overlay-for-vpc-peer-routing/terraform/main.tf
@@ -3,8 +3,8 @@ locals {
         for environment in keys(var.environments):[
             for peer in var.environments[environment].vpc_peers: {
                 name="${environment}-to-${peer}"
-                local=module.project_standup[environment].network_self_link
-                remote=module.project_standup[peer].network_self_link
+                local=module.environment_standup[environment].network_self_link
+                remote=module.environment_standup[peer].network_self_link
             }
         ]
     ])
@@ -12,16 +12,16 @@ locals {
         for vpn_peers in var.vpn_peerings: {          
             first = {
                 name = vpn_peers.first,
-                network = module.project_standup[vpn_peers.first].network_self_link,
-                project_id = module.project_standup[vpn_peers.first].project_id,
-                region =  module.project_standup[vpn_peers.first].region,
+                network = module.environment_standup[vpn_peers.first].network_self_link,
+                project_id = module.environment_standup[vpn_peers.first].project_id,
+                region =  module.environment_standup[vpn_peers.first].region,
                 advertised_networks = {"10.0.0.0/8"="RFC-1918","172.16.0.0/12"="RFC-1918","192.168.0.0/16"="RFC-1918"},
             },
             second = {
                 name = vpn_peers.second,
-                network = module.project_standup[vpn_peers.second].network_self_link,
-                project_id = module.project_standup[vpn_peers.second].project_id,
-                region =  module.project_standup[vpn_peers.second].region,
+                network = module.environment_standup[vpn_peers.second].network_self_link,
+                project_id = module.environment_standup[vpn_peers.second].project_id,
+                region =  module.environment_standup[vpn_peers.second].region,
                 advertised_networks = {(var.environments[vpn_peers.second].gke_master_net)="GKE Master Network"}
             }
         }
@@ -34,7 +34,7 @@ module "environment_standup" {
 
     for_each = var.environments
     environment = each.key
-    
+
     org_id = try(var.organization_id,null)
     folder_id = try(var.folder_id,null)
     project_id = try(var.project_id,null)
@@ -57,11 +57,11 @@ module "jump_host" {
     for_each = var.environments
     
     environment = each.key
-    vpc_name = module.project_standup[each.key].network_self_link
-    project_id = module.project_standup[each.key].project_id
-    random_id = module.project_standup[each.key].id
-    subnetwork = module.project_standup[each.key].subnetwork_self_link
-    region =  module.project_standup[each.key].region    
+    vpc_name = module.environment_standup[each.key].network_self_link
+    project_id = module.environment_standup[each.key].project_id
+    random_id = module.environment_standup[each.key].id
+    subnetwork = module.environment_standup[each.key].subnetwork_self_link
+    region =  module.environment_standup[each.key].region    
 }
 
 module "gke_cluster" {
@@ -69,12 +69,12 @@ module "gke_cluster" {
     for_each = var.environments
 
     environment = each.key
-    vpc_name = module.project_standup[each.key].network_self_link
-    project_id = module.project_standup[each.key].project_id
-    random_id = module.project_standup[each.key].id
-    subnetwork = module.project_standup[each.key].subnetwork_self_link
+    vpc_name = module.environment_standup[each.key].network_self_link
+    project_id = module.environment_standup[each.key].project_id
+    random_id = module.environment_standup[each.key].id
+    subnetwork = module.environment_standup[each.key].subnetwork_self_link
     gke_master_net = each.value.gke_master_net
-    region =  module.project_standup[each.key].region       
+    region =  module.environment_standup[each.key].region       
 }
 
 module "vpn_peering" {
