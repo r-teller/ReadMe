f3fb057936014025169d58bb7325b078f4cf3624
Robert Teller <robert.j.c.teller@gmail.com>
2021-03-01 07:45:49 -0800

    updating peering

--- google-cloud/1-vpn-overlay-for-vpc-peer-routing/terraform/modules/2_vpc_peering/main.tf
+++ google-cloud/1-vpn-overlay-for-vpc-peer-routing/terraform/modules/2_vpc_peering/main.tf
@@ -4,4 +4,7 @@ resource "google_compute_network_peering" "peering" {
   name         = each.key
   network      = each.value.local
   peer_network = each.value.remote
+  
+  import_custom_routes = true
+  export_custom_routes = true
 }
